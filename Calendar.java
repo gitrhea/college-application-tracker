3201a0c779f307e244ca4e942ce2413d18b6624b
gitrhea <61709204+gitrhea@users.noreply.github.com>
2020-04-17 08:54:34 -0400

    Update Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -1,12 +1,26 @@
+import java.util.*;
+// The calendar class is an abstract class. A constructor is not used to create an instance. Instead, a static method is used. 
+
 public class Calendar{
-    private int year = 2020;
-    private int month = 1;
-    private String monthName = "January";
-    private int day = 1;
+    
+    // private int year = 2020;
+    // private int month = 4;
+    // private String monthName = "April";
+    // private int day = 16;
 
-    public int findYear(){
-        //takes user input and assigns it to year
-    }
+    // public int findYear(){
+    //     //takes user input and assigns it to year
+    // } 
 
-    public 
+    // public void setDate(){
+    //     Date = 
+    // }
+
+    // public Calendar viewCalendar(){
+    //     //code that helps us view calendar
+    // }
+
+    // public void plot(){
+
+    // }
 }
