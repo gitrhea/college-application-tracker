8c9a63ee9cb7ebeab9d259391ef226b4d02d80c6
gitrhea <61709204+gitrhea@users.noreply.github.com>
2020-04-17 08:56:21 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,6 @@
-import java.util*;
-
 public class Main{
     public static void main (String[] args){
-        College UF = new College("The University of Florida", "public", "large", "Gainseville, Florida");
-        System.out.println(UF);
-
-        Scanner yesOrNo = new Scanner;
-        System.out.println("Will you be applying for the 2020-2021 application cycle?");
-        String yesOrNo = inputYesOrNo.nextLine();
-
-        if (yesOrNo.equals("yes") || yesOrNo.equals("Yes")){
-            System.out.println("Great!");
-        }
+        Calendar c = Calendar.getInstance();  
+        System.out.println("The Current Date is:" + c.getTime());  
     }
 }
