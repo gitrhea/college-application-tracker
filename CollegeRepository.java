cb85a7ea77080000027e9335d668e46a3bff21fd
gitrhea <61709204+gitrhea@users.noreply.github.com>
2020-04-17 08:55:54 -0400

    Add files via upload

---
+++ CollegeRepository.java
@@ -0,0 +1,6 @@
+public class CollegeRepository extends College{
+    private College[] repository = {
+        new College("University of Florida", "UF", "public", "large", "Gainseville", "Common Application", "SAT and ACT", true, false, true),
+        new College("University of Southern California", "USC", "private", "large", "Los Angeles", "Common Application", "SAT and ACT", true, false, true)
+    };
+}
