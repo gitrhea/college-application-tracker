3a78c63a44b9344acea6a004f06550a176f02e08
gitrhea <61709204+gitrhea@users.noreply.github.com>
2020-04-16 22:03:28 -0400

    Update College.java

--- College.java
+++ College.java
@@ -1,24 +1,35 @@
-public class College{
-    private String name;
-    private String publicOrPrivate;
-    private String size;
-    private String location;
-    /**private int deadlineMonth;
-    private int deadlineDay;
-    private String SATorACT;
-    private boolean requiresEssays;
-    private boolean requiresInterview;
-    **/
+import java.util.*;
 
-    public College(String name, String publicOrPrivate, String size, String location){
-        this.name = name;
+public class College{
+    private String formalName = "";
+    private String abbrevName = "";
+    private String publicOrPrivate = "";
+    private String size = ""; //small, medium, large
+    private String location = "";
+    private String applicationType = "";
+    private Date deadline = ""; //import Date class
+    private String SATorACT = "";
+    private boolean requiresSuppEssays = false;
+    private boolean requiresInterview = false;
+    private boolean scholarshipOpportunities = false;
+    private String extraNotes = "";
+
+    public College(String formalName, String abbrevName, String publicOrPrivate, String size, String location, String applicationType, String SATorACT, boolean requiresSuppEssays, boolean requiresInterview, boolean scholarshipOpportunities, String extraNotes){
+        this.formalName = formalName;
+        this.abbrevName = abbrevName;
         this.publicOrPrivate = publicOrPrivate;
         this.size = size;
         this.location = location;
+        this.applicationType = applicationType;
+        this.SATorACT = SATorACT;
+        this.requiresSuppEssays = requiresSuppEssays;
+        this.requiresInterview = requiresInterview;
+        this.scholarshipOpportunities = scholarshipOpportunities;
+        this.extraNotes = extraNotes;
     }
 
     public String toString(){
-        return name + " is a " + size + " " + publicOrPrivate + " institution located in " + location + ".";
+        return formalName + " is a " + size + " " + publicOrPrivate + " institution located in " + location + ".";
     }
 
 }
